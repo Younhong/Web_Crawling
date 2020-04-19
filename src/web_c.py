e57d6e5c40e5e7472e3cb9c8255bbc1b2007a5b7
Younhong <younhong@kakao.com>
2020-04-19 18:20:51 +0900

    1

--- src/web_c.py
+++ src/web_c.py
@@ -4,10 +4,6 @@ import xmltodict
 
 keyword = input("Enter keyword: ")
 
-### If you enter anything other than clothes, exit
-if (keyword != '의류'):
-        exit(0)
-
 url = 'http://openapi.11st.co.kr/openapi/OpenApiService.tmall'
 
 params = {'key' : 'use your own key', 
